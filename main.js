e1f3490f2c09389293b1662862206499aed0bfa8
Patrik Hammersborg <patrikham0@gmail.com>
2018-12-30 21:05:23 +0100

    Better structure and improvements

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var peer = new Peer();
-
-peer.on('open', function (id) {
-    console.log('My peer ID is: ' + id);
+//might be able to find this on the "peer"-object
+var ownId;
+peer.on("open", function (id) {
+    console.log("My peer ID is: " + id);
+    ownId = id;
+    updateOwnId(id);
 });
 var peer;
 var conn;
@@ -10,36 +13,63 @@ var conn;
 peer.on("connection", function (new_conn) {
     conn = new_conn;
     console.log(new_conn);
-    update_connected_to(new_conn.peer);
+    updateConnectedTo(conn.peer);
+    prepareMessaging();
     conn.on("data", function (data) {
-        show(data.message);
+        displayMessage(data.message, data.timestamp, conn.peer);
     });
 });
 
+//TODO: should set labels by hand (choose nicknames)
+//TODO: should also get references to all html-elements once, and save these somewhere
 function connect() {
-    var target = $("#target-id").first().val();
+    var target = $("#target-id").val();
     conn = peer.connect(target);
     conn.on("open", function () {
-        update_connected_to(target);
+        updateConnectedTo(target);
+        prepareMessaging();
         conn.on("data", function (data) {
-            show(data.message);
+            displayMessage(data.message, data.timestamp, conn.peer);
         });
+        console.log(conn);
     });
+}
+
+//More here later
+function prepareMessaging() {
+    enableButtonGroup("connection-buttons", false);
+    enableButtonGroup("messaging-buttons", true);
 }
 
 function send() {
     if (peer == null || conn == null) {
         return;
     }
-    var message = $("#message").first().val();
-    conn.send({ test: 1, message: message });
+    var message = $("#message").val();
+    $("#message").val("");
+    displayMessage(message, currentTimestamp(), ownId);
+    conn.send({ test: 1, message: message, timestamp: currentTimestamp() });
 }
 
-function show(data) {
-    console.log(data);
-    $("#recieved-messages").first().innerHTML = data;
+// Could go in own file
+function enableButtonGroup(name, state) {
+    $("#" + name).prop("disabled", !state);
 }
 
-function update_connected_to(name) {
-    $("#connected-to").first().text(name);
+
+function displayMessage(text, timestamp, id) {
+    $("#messages").append("<p><b>" + id + "</b>" + ": " + text + " - <i>" + timestamp + "</i></p>");
+}
+
+function updateConnectedTo(name) {
+    $("#connected-to").text(name);
+}
+
+function updateOwnId(id) {
+    $("#my-id").text(id);
+}
+
+function currentTimestamp() {
+    var date = new Date();
+    return date.toLocaleTimeString();
 }
